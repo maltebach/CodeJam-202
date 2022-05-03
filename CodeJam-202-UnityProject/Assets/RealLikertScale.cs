927ec63c59976e1f3c079dc20badf8de32fa7eb9
hjalteklarsen <hjalteklarsen@gmail.com>
2022-05-03 10:25:07 +0200

    LikertScale

--- CodeJam-202-UnityProject/Assets/RealLikertScale.cs
+++ CodeJam-202-UnityProject/Assets/RealLikertScale.cs
@@ -12,6 +12,7 @@ public class RealLikertScale : MonoBehaviour
     public Transform likert6;
     public Transform likert7;
     public int cursor = -1;
+    Vector3 originalSize = new Vector3 (0.65285f, 0.65285f, 0.65285f);
 
     public void SetLikert(int i)
     {
@@ -19,25 +20,25 @@ public class RealLikertScale : MonoBehaviour
         switch(i)
         {
             case 1:
-            likert1.localScale = new Vector3(1.2f, 1.2f, 1.2f);
+            likert1.localScale = new Vector3(0.8f, 0.8f, 0.8f);
             break;
             case 2:
-            likert2.localScale = new Vector3(1.2f, 1.2f, 1.2f);
+            likert2.localScale = new Vector3(0.8f, 0.8f, 0.8f);
             break;
             case 3:
-            likert3.localScale = new Vector3(1.2f, 1.2f, 1.2f);
+            likert3.localScale = new Vector3(0.8f, 0.8f, 0.8f);
             break;
             case 4:
-            likert4.localScale = new Vector3(1.2f, 1.2f, 1.2f);
+            likert4.localScale = new Vector3(0.8f, 0.8f, 0.8f);
             break;
             case 5:
-            likert5.localScale = new Vector3(1.2f, 1.2f, 1.2f);
+            likert5.localScale = new Vector3(0.8f, 0.8f, 0.8f);
             break;  
             case 6:
-            likert6.localScale = new Vector3(1.2f, 1.2f, 1.2f);
+            likert6.localScale = new Vector3(0.8f, 0.8f, 0.8f);
             break;  
             case 7:
-            likert7.localScale = new Vector3(1.2f, 1.2f, 1.2f);
+            likert7.localScale = new Vector3(0.8f, 0.8f, 0.8f);
             break;  
             default:
 
@@ -48,12 +49,12 @@ public class RealLikertScale : MonoBehaviour
     }
     private void ResetScale()
     {
-        likert1.localScale = Vector3.one;
-        likert2.localScale = Vector3.one;
-        likert3.localScale = Vector3.one;
-        likert4.localScale = Vector3.one;
-        likert5.localScale = Vector3.one;
-        likert6.localScale = Vector3.one;
-        likert7.localScale = Vector3.one;
+        likert1.localScale = originalSize;
+        likert2.localScale = originalSize;
+        likert3.localScale = originalSize;
+        likert4.localScale = originalSize;
+        likert5.localScale = originalSize;
+        likert6.localScale = originalSize;
+        likert7.localScale = originalSize;
     }
 }
