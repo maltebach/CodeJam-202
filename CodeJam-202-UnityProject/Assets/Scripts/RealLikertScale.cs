63c27a6e0e0e3e02dde2f214c6618a382f364121
hjalteklarsen <hjalteklarsen@gmail.com>
2022-05-04 10:28:13 +0200

    Likert works

--- CodeJam-202-UnityProject/Assets/Scripts/RealLikertScale.cs
+++ CodeJam-202-UnityProject/Assets/Scripts/RealLikertScale.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RealLikertScale : MonoBehaviour
 {
@@ -13,32 +14,53 @@ public class RealLikertScale : MonoBehaviour
     public Transform likert7;
     public int cursor = -1;
     Vector3 originalSize = new Vector3 (0.65285f, 0.65285f, 0.65285f);
+    public Image limg1;
+    public Image limg2;
+    public Image limg3;
+    public Image limg4;
+    public Image limg5;
+    public Image limg6;
+    public Image limg7;
+    public Color red;
+    private Color originalColor; 
 
+    private void Start() 
+    {
+        originalColor = limg1.color;
+    }
     public void SetLikert(int i)
     {
         ResetScale();
+        ResetColor();
         switch(i)
         {
             case 1:
             likert1.localScale = new Vector3(0.8f, 0.8f, 0.8f);
+            limg1.color = red;
             break;
             case 2:
             likert2.localScale = new Vector3(0.8f, 0.8f, 0.8f);
+            limg2.color = red;
             break;
             case 3:
             likert3.localScale = new Vector3(0.8f, 0.8f, 0.8f);
+            limg3.color = red;
             break;
             case 4:
             likert4.localScale = new Vector3(0.8f, 0.8f, 0.8f);
+            limg4.color = red;
             break;
             case 5:
             likert5.localScale = new Vector3(0.8f, 0.8f, 0.8f);
+            limg5.color = red;
             break;  
             case 6:
             likert6.localScale = new Vector3(0.8f, 0.8f, 0.8f);
+            limg6.color = red;
             break;  
             case 7:
             likert7.localScale = new Vector3(0.8f, 0.8f, 0.8f);
+            limg7.color = red;
             break;  
             default:
 
@@ -56,5 +78,15 @@ public class RealLikertScale : MonoBehaviour
         likert5.localScale = originalSize;
         likert6.localScale = originalSize;
         likert7.localScale = originalSize;
+    }
+    private void ResetColor()
+    {
+        limg1.color = originalColor;
+        limg2.color = originalColor;
+        limg3.color = originalColor;
+        limg4.color = originalColor;
+        limg5.color = originalColor;
+        limg6.color = originalColor;
+        limg7.color = originalColor;
     }
 }
